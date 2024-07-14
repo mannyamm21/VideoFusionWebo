2bbe24f3d8577c2250be44dcca415e06497cd538
mannyamm21 <130400515+mannyamm21@users.noreply.github.com>
2024-07-15 00:14:59 +0530

    new

--- Backend/src/middleware/verifyToken.js
+++ Backend/src/middleware/verifyToken.js
@@ -3,8 +3,8 @@ import { ApiError } from '../utils/ApiError.js';
 import { asyncHandler } from '../utils/asyncHandler.js'
 
 export const verifyToken = asyncHandler(async (req, res, next) => {
-    const token = req.cookies?.access_token || req.header("Authorization")?.replace("Bearer ", "")
-    // const token = req.cookies?.access_token;
+    // const token = req.cookies?.access_token || req.header("Authorization")?.replace("Bearer ", "")
+    const token = req.cookies.access_token;
     if (!token) throw new ApiError(401, "Unauthorized request")
 
     jwt.verify(token, process.env.JWT, (err, user) => {
