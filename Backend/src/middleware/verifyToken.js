9db6437a402429b17a8d2ac153a0aa477d6262b9
mannyamm21 <130400515+mannyamm21@users.noreply.github.com>
2024-07-16 22:11:32 +0530

    new

--- Backend/src/middleware/verifyToken.js
+++ Backend/src/middleware/verifyToken.js
@@ -5,7 +5,7 @@ import { User } from '../models/User.js';
 
 export const verifyToken = asyncHandler(async (req, res, next) => {
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        const token = req.cookies.jwt;
 
         console.log('Token:', token); // Check the extracted token
         if (!token) {
