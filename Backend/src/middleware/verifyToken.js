94a944ae3c4a93658c3f56c0d81884e82902773c
mannyamm21 <130400515+mannyamm21@users.noreply.github.com>
2024-07-17 14:25:26 +0530

    new

--- Backend/src/middleware/verifyToken.js
+++ Backend/src/middleware/verifyToken.js
@@ -11,7 +11,7 @@ export const verifyToken = asyncHandler(async (req, res, next) => {
 
         if (!token) {
             return res.status(401).json({ message: 'Token is missing' });
-        } ACCESS_TOKEN_SECRET
+        }
 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
